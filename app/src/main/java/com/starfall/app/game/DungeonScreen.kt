093ac3806b5847cfee31890cefa39c339d02dd1a
twig21337 <wade.twig@gmail.com>
2025-11-20 16:04:10 -0800

    Fix matchParentSize import for dungeon tiles

--- app/src/main/java/com/starfall/app/game/DungeonScreen.kt
+++ app/src/main/java/com/starfall/app/game/DungeonScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.grid.GridCells
