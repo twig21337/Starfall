3d1c183c693c34b84952b309c3d0e724ed95a6be
twig21337 <wade.twig@gmail.com>
2025-11-24 17:50:38 -0800

    Add missing clickable import

--- app/src/main/java/com/starfall/app/game/DungeonScreen.kt
+++ app/src/main/java/com/starfall/app/game/DungeonScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
